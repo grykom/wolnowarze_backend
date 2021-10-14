71ba00af83c85cb4c1bce5eb3e2495bfa95ef778
Radoslaw Neumann <grykom@gmail.com>
2021-10-14 13:38:57 +0200

    14.10

--- api/serializers.py
+++ api/serializers.py
@@ -6,14 +6,18 @@ class ReceipeSerializer(serializers.ModelSerializer):
     images = serializers.StringRelatedField(many=True)
     class Meta:
         model = Receipe
-        fields = ['receipe_id', 'name', 'serving_size', 'preparing_time', 'time_on_high', 'time_on_low', 'images']
+        fields = ['receipe_id', 'name',
+            'serving_size', 'preparing_time',
+            'time_on_high', 'time_on_low',
+            'receipe_ingredients', 'receipe_how_to',
+            'images']
 
 
 class ReceipeGallerySerializer(serializers.ModelSerializer):
     images = serializers.StringRelatedField(many=True)
     class Meta:
         model = Receipe
-        fields = ['id', 'name', 'images']
+        fields = ['receipe_id', 'name', 'images']
 
 
 class WhySlowcookerSerializer(serializers.ModelSerializer):
