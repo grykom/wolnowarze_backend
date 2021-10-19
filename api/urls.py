8df83a2ae2bc661a69538f8748f39fc9f9f63ed4
grykom <grykom@gmail.com>
2021-10-19 17:46:39 +0200

    19.10

--- api/urls.py
+++ api/urls.py
@@ -1,16 +1,33 @@
 from django.urls import path
 from . import views, cron
+
 app_name = "api"
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("api/cron/pull-data", cron.cron_pull_data),
-
-    path("v1/no_idea_receipes", views.NoIdeaReceipesView.as_view({'get': 'list'}), name="no_idea_receipes"),
-    path("v1/why_slowcooker", views.WhySlowcookerView.as_view({'get': 'list'}), name="why_slowcooker"),
-    path("v1/gallery", views.GalleryView.as_view({'get': 'list'}), name="gallery"),
-
-    path("v1/receipes", views.ReceipesView.as_view({'get': 'list'}), name="receipes_list"),
-    path("v1/receipes/<int:receipe_id>", views.ReceipesView.as_view({'get': 'retrieve'}), name="receipe"),
-    path("v1/receipes/<int:receipe_id>/likes_<str:up_down>", views.ReceipesView.as_view({'post': 'likes'}), name="receipe_likes"),
+    path(
+        "v1/no_idea_receipes",
+        views.NoIdeaReceipesView.as_view({"get": "list"}),
+        name="no_idea_receipes",
+    ),
+    path(
+        "v1/why_slowcooker",
+        views.WhySlowcookerView.as_view({"get": "list"}),
+        name="why_slowcooker",
+    ),
+    path("v1/gallery", views.GalleryView.as_view({"get": "list"}), name="gallery"),
+    path(
+        "v1/receipes", views.ReceipesView.as_view({"get": "list"}), name="receipes_list"
+    ),
+    path(
+        "v1/receipes/<int:receipe_id>/",
+        views.ReceipesView.as_view({"get": "retrieve"}),
+        name="receipe",
+    ),
+    path(
+        "v1/receipes/<int:receipe_id>/likes_<str:up_down>",
+        views.ReceipesView.as_view({"post": "likes"}),
+        name="receipe_likes",
+    ),
 ]
